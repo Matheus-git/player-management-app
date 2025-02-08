1f1f8c3f5a00213989662e350a2642116b176c35
Matheus-git <mathiew0@gmail.com>
2025-02-08 14:51:11 -0300

    install tailwindui

--- src/components/ui/Header/header.tsx
+++ src/components/ui/Header/header.tsx
@@ -1,3 +1,8 @@
 export const Header = () => {
-  return <>Cabeçalho</>;
+  return (
+    <div className="w-screen h-20 bg-black flex justify-center items-center">
+      <image />
+      <h1 className="text-white">Drakkar Squad</h1>
+    </div>
+  );
 };
